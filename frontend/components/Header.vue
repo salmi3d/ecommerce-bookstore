fd61f790e490e58441db2ebc724d4fe1a7cd131a
salmi3d <salmi3d@gmail.com>
2020-01-20 16:20:36 +0200

    added header component

---
+++ frontend/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <client-only>
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+                <li class="uk-active"><router-link tag="a" class="navbar-brand" to="/" exact>BOOKSTORE</router-link></li>
+                <li><router-link tag="a" class="navbar-brand" to="/categories" exact>Categories</router-link></li>
+            </ul>
+        </div>
+    </nav>
+  </client-only>
+</template>

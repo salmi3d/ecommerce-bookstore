30e4de4058d9593b9087759c5684696d63e25cf7
salmi3d <salmi3d@gmail.com>
2020-01-21 10:58:29 +0200

    added auth store module

---
+++ frontend/store/auth.js
@@ -0,0 +1,10 @@
+import Cookies from 'js-cookie'
+
+export const state = () => {}
+
+export const mutations = {
+  setUser(state, user) {
+    state.user = user
+    Cookies.set('user', user)
+  }
+}
